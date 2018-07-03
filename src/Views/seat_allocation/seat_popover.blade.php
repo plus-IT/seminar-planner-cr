d45e91606c02af87dba9aa748c2749951cc824c7
Bindesh Pandya <bindeshpandya@hotmail.com>
2018-07-03 17:48:53 +0530

    translation changes

--- src/Views/seat_allocation/seat_popover.blade.php
+++ src/Views/seat_allocation/seat_popover.blade.php
@@ -1,10 +1,10 @@
 <div class="frankfurt_pophover">
-    <p><i class="fa seat_small" aria-hidden="true"></i>Seats assigned to Organisation:
+    <p><i class="fa seat_small" aria-hidden="true"></i>{!! Html::customTrans("seminarPlanner.seats_assigned_organization") !!} :
         <span> ({!! $allocation->name !!} ({!! !empty($allocation->getAttendees)?$allocation->getAttendees->count() :0 !!}/{!! $allocation->allocatedSeat !!})</span></p>
-    <p><i class="fa fa-user" aria-hidden="true"></i>Assigned by:
+    <p><i class="fa fa-user" aria-hidden="true"></i>{!! Html::customTrans("seminarPlanner.assigned_by") !!} :
         <span>{!!  !empty($allocation->FirstName) ?$allocation->FirstName .' '.$allocation->LastName:'' !!}</span></p>
-    <p><i class="fa fa-calendar-o" aria-hidden="true"></i>Assigned on:
+    <p><i class="fa fa-calendar-o" aria-hidden="true"></i>{!! Html::customTrans("seminarPlanner.assigned_on") !!} :
         <span>{!! !empty($allocation->assginedDate)? format_date($allocation->assginedDate):'' !!}</span></p>
     <hr/>
-    <p>Participant is already assigned from {!! $allocation->name !!} </p>
+    <p>{!! Html::customTrans("seminarPlanner.participant_already_registred") !!} {!! $allocation->name !!} </p>
 </div>
